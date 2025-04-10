241a99e419677fde1b53d61a58a1c7b242f8cc5f
ashishkhadye33 <ashishkhadye33@gmail.com>
2025-04-10 12:47:09 +0530

    dotnet project day 1

---
+++ EMS/Profile.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EMS
+{
+    public partial class Profile : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connstr"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["EmployeeId"]!=null)
+            {
+                if(!IsPostBack)
+                {
+
+                getEmployeeProfile();
+                }
+            }
+            else
+            {
+                Response.Redirect("Login.aspx");
+            }
+        }
+
+        protected void getEmployeeProfile()
+        {
+            con.Close();
+            SqlCommand cmd = new SqlCommand("select * from EmpRegister where Srno=@id", con);
+            cmd.Parameters.AddWithValue("@id", Session["EmployeeId"]);
+            con.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            if (sdr.HasRows)
+            {
+                sdr.Read();
+                txtName.Text = sdr.GetValue(1).ToString();
+                txtEmail.Text = sdr.GetValue(2).ToString();
+                txtContact.Text = sdr.GetValue(3).ToString();
+                txtdept.Text = sdr.GetValue(4).ToString();
+            }
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            con.Close();
+            SqlCommand cmd = new SqlCommand("update EmpRegister set Name=@name,Email=@email,Contact=@contact where Srno=@id",con);
+            cmd.Parameters.AddWithValue("@name",txtName.Text);
+            cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+            cmd.Parameters.AddWithValue("@contact", txtContact.Text);
+            cmd.Parameters.AddWithValue("@id", Session["EmployeeId"]);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            getEmployeeProfile();
+        }
+    }
+}
